a80df75b88f194aac455c2a806fd75bfba673d34
mmpriebe <marciano.priebe@gmail.com>
2026-07-08 08:38:25 -0300

    adicionado imagem de banner

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,15 @@ import "./App.css";
 function App() {
 
   return (
-    <Navbar />
+    <>
+      <Navbar />
+
+      <div className="centered-content">
+        <h3>Bem-vindo à nossa página!</h3>
+        <p>Esta é a página inicial do nosso aplicativo React.</p>
+        <img src="/placeholder.svg" alt="Placeholder" className="placeholder-image" />
+      </div>
+    </>
   );
 }
 
